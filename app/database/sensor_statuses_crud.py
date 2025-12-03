3ee8e194150855c39d1f95a0f7e9d357de2c00ab
Ekku Lehto <ekkulehto@proton.me>
2025-12-03 21:39:23 +0200

    fix: Change datatypes to from sensor to sensor_status

--- app/database/sensor_statuses_crud.py
+++ app/database/sensor_statuses_crud.py
@@ -1,6 +1,6 @@
 from fastapi import HTTPException, Response, status
 from sqlmodel import Session, select
-from .models import SensorStatusIn, SensorStatusDb, SensorDb
+from .models import SensorStatusIn, SensorStatusDb
 
 def create_sensor_status(session: Session, sensor_status_in: SensorStatusIn):
     sensor_status = SensorStatusDb.model_validate(sensor_status_in)
@@ -12,16 +12,16 @@ def create_sensor_status(session: Session, sensor_status_in: SensorStatusIn):
 def get_all_sensor_statuses(session: Session):
     return session.exec(select(SensorStatusDb)).all()
 
-def get_sensor_status_by_id(session: Session, sensor_id: int):
-    sensor = session.get(SensorDb, sensor_id)
+def get_sensor_status_by_id(session: Session, sensor_status_id: int):
+    sensor_status = session.get(SensorStatusDb, sensor_status_id)
 
-    if not sensor:
+    if not sensor_status:
         raise HTTPException(
             detail='Sensor not found',
             status_code=status.HTTP_404_NOT_FOUND
         )
     
-    return sensor
+    return sensor_status
 
 def delete_sensor_status_by_id(session: Session, sensor_status_id: int):
     sensor_status = session.get(SensorStatusDb, sensor_status_id)
