64b19720d5bc0d6a126007cc1b4b8fa820b97870
Ekku Lehto <ekkulehto@proton.me>
2025-12-05 14:21:16 +0200

    feat: Change measurement output-models, tahs and return 204 straight from delete without body

--- app/routers/measurements.py
+++ app/routers/measurements.py
@@ -3,22 +3,22 @@ from sqlmodel import Session
 from ..database.database import get_session
 
 from ..database import measurements_crud as crud
-from ..database.models import MeasurementIn, MeasurementDb
+from ..database.models import MeasurementIn, MeasurementDb, MeasurementOut, MeasurementOutWithSensor
 
-router = APIRouter(prefix='/measurements', tags=['measurements'])
+router = APIRouter(prefix='/measurements', tags=['Measurements'])
 
-@router.get('', response_model=list[MeasurementDb])
+@router.get('', response_model=list[MeasurementOutWithSensor])
 def get_all_measurements(*, session: Session = Depends(get_session)):
     return crud.get_all_measurements(session)
 
-@router.get('/{measurement_id}', response_model=MeasurementDb)
+@router.get('/{measurement_id}', response_model=MeasurementOutWithSensor)
 def get_measurement_by_id(*, session: Session = Depends(get_session), measurement_id: int):
     return crud.get_measurement_by_id(session, measurement_id)
 
-@router.post("", status_code=status.HTTP_201_CREATED, response_model=MeasurementDb)
+@router.post('', status_code=status.HTTP_201_CREATED, response_model=MeasurementOutWithSensor)
 def create_measurement(*, session: Session = Depends(get_session), measurement_in: MeasurementIn):
     return crud.create_measurement(session, measurement_in)
 
-@router.delete("/{measurement_id}", response_model=MeasurementDb)
+@router.delete('/{measurement_id}', status_code=status.HTTP_204_NO_CONTENT)
 def delete_measurement_by_id(*, session: Session = Depends(get_session), measurement_id: int):
     return crud.delete_measurement_by_id(session, measurement_id)
