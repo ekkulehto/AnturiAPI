b024def21610f548af18816b67bc761f786147f9
Ekku Lehto <ekkulehto@proton.me>
2025-12-09 14:18:36 +0200

    refactor/feat: Import new refactored routers

--- src/main.py
+++ src/main.py
@@ -5,7 +5,8 @@ from .database import create_db
 from .segments.router import router as segments_router
 from .sensors.router import router as sensors_router
 from .sensor_status.router import router as sensor_status_router
-from .sensor_measurements.router import router as measurements_router
+from .sensor_measurements.router import router as sensor_measurements_router
+from .measurements.router import router as measurements_router
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -17,4 +18,5 @@ app = FastAPI(lifespan=lifespan)
 app.include_router(segments_router)
 app.include_router(sensors_router)
 app.include_router(sensor_status_router)
+app.include_router(sensor_measurements_router)
 app.include_router(measurements_router)
